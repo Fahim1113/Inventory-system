974c61a34d6ed090bd34fb8afe3f696d8b784c3b
Fahim1113 <fahimshahriyarc@gmail.com>
2022-09-30 21:43:56 +0100

    Finishedgit add .!

--- frontend/src/GlobalState.js
+++ frontend/src/GlobalState.js
@@ -3,6 +3,6 @@ import { createGlobalState } from "react-hooks-global-state";
 const { getGlobalState, setGlobalState } = createGlobalState({
   username: "",
   password: "",
-  url: "http://localhost:4000",
+  url: "https://inventorySystem.fahim-shahriyar.repl.co",
 });
 export { getGlobalState, setGlobalState };
