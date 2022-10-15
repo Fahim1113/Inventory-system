23cced91363fa7606be8a52bbe4c583b9bfb8fdc
Fahim1113 <fahimshahriyarc@gmail.com>
2022-10-15 13:20:36 +0100

    fixed jdenticon error

--- frontend/src/GlobalState.js
+++ frontend/src/GlobalState.js
@@ -3,7 +3,7 @@ import { createGlobalState } from "react-hooks-global-state";
 const { getGlobalState, setGlobalState } = createGlobalState({
   username: "",
   password: "",
-  url: "http://192.168.0.15:4000",
+  url: "http://localhost:4000",
   url1: "https://inventorySystem.fahim-shahriyar.repl.co",
 });
 export { getGlobalState, setGlobalState };
